8a6092b93b7dfbbe89ed22508abc8502749fbae3
akbaradiw <akbaradiw000@gmail.com>
2024-06-17 09:28:40 +0700

    add link to navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,7 +7,9 @@ const Navbar = () => {
   return (
     <nav className="bg-blue-700 p-4 mb-4">
       <div className="container mx-auto flex items-center 0 justify-between">
-        <h1 className='text-white text-2xl font-bold'> Anime List</h1>
+        <Link to='/'>
+        <button className='text-white text-2xl font-bold hover:cursor-pointer'> Anime List</button>
+        </Link>
       </div>
  
     </nav>
